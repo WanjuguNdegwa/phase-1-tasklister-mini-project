0803c111ec0d04040ad998dd32263df275d9cf68
WanjuguNdegwa <99987352+WanjuguNdegwa@users.noreply.github.com>
2022-04-18 19:54:15 +0300

    Added ability to add task to list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form')
+  const tasks = document.getElementById('tasks')
+  const input = document.getElementById('new-task-description');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const taskDescription = input.value;
+
+    if (taskDescription.length > 0) {
+      const li = document.createElement('li');
+      li.textContent = taskDescription;
+      tasks.appendChild(li)
+    }
+  })
 });
